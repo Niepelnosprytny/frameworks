3ac92c5769ebaf85b768604921987299a31ba823
Sebastian Konkol <sebastian.konkol2206@gmail.com>
2022-03-12 12:20:45 +0100

    pierwsze zadanie

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,7 +2,33 @@
 
 namespace App\Controller;
 
-class BlogController
-{
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+class BlogController extends AbstractController
+{
+    #[Route('/blog', name: 'app_blog')]
+    public function index(): Response
+    {
+        return $this->render('blog/index.html.twig');
+    }
+
+    #[Route('/blog/article', name: 'app_blog_article')]
+    public function article(): Response
+    {
+        return $this->render('blog/article.html.twig');
+    }
+
+    #[Route('/blog/news', name: 'app_blog_news')]
+    public function news(): Response
+    {
+        return $this->render('blog/news.html.twig');
+    }
+
+    #[Route('/blog/random', name: 'app_blog_random')]
+    public function random(): Response
+    {
+        return $this->render('blog/random.html.twig');
+    }
 }
