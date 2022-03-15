affbf31b83ca7c8e36a9880dcb9e4356612b1d2e
Sebastian Konkol <sebastian.konkol2206@gmail.com>
2022-03-15 11:40:23 +0100

    druga lekcja - poprawiona

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -11,7 +11,7 @@ class BlogController extends AbstractController
     #[Route('/blog', name: 'app_blog')]
     public function index(): Response
     {
-        return $this->render('blog/index.html.twig');
+        return $this->render('blog/index.html.twig', ['isHomepage' => true]);
     }
 
     #[Route('/blog/article', name: 'app_blog_article')]
